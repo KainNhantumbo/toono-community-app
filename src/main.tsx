1f410e72ed2795e9c6bb8c0084d49cdb26636e1f
Kain Nhantumbo <nhantumbok@gmail.com>
2024-04-18 21:41:52 +0200

    feat: added reducer config.

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,16 @@ import '@/styles/index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { AppRouter } from './app.tsx';
+import { Provider as StoreProvider } from 'react-redux';
 import { AppContext } from './context/app-context.tsx';
+import { store } from './state/store.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AppContext>
-      <AppRouter />
-    </AppContext>
+    <StoreProvider store={store}>
+      <AppContext>
+        <AppRouter />
+      </AppContext>
+    </StoreProvider>
   </React.StrictMode>
 );
